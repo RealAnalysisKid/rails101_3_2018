a5c58cbadbeea82b7732a63ddbd2b5c9f711446c
Xin <Detective_boys@hotmil.com>
2018-12-16 18:06:16 +0800

    8.3 Homework first attampt

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
+    find_post_and_check_permission
   end
 
   def create
@@ -18,7 +22,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    find_post_and_check_permission
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+
+  end
+
   private
+
+  def find_post_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if current_user =! @post.user
+      redirect_to root_path, alert: "You have no permission"
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
